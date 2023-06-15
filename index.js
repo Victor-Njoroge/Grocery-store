cabbf5118647ef5b15ce1e7021d8399de52ad37e
Victor <victornjoroge2020@gmail.com>
2023-06-15 18:11:34 +0300

    First commit

---
+++ index.js
@@ -0,0 +1,9 @@
+let searchForm = document.querySelector('.search-form');
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active')
+}
+
+let shoppingCart = document.querySelector('.shopping-cart');
+document.querySelector('#cart-btn').onclick = () =>{
+    shoppingCart.classList.toggle('active')
+}
