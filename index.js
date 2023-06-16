47ca41734800a12cdf03621e543755e2b6be667f
Victor <victornjoroge2020@gmail.com>
2023-06-16 17:02:12 +0300

    3rd Commit

--- index.js
+++ index.js
@@ -7,3 +7,8 @@ let shoppingCart = document.querySelector('.shopping-cart');
 document.querySelector('#cart-btn').onclick = () =>{
     shoppingCart.classList.toggle('active')
 }
+
+let loginForm = document.querySelector('.login-form');
+document.querySelector('#login-btn').onclick = () =>{
+    loginForm.classList.toggle('active')
+}
