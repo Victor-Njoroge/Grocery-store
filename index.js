af28cb8bd8d552d16f02d777e26f752741229e78
Victor <victornjoroge2020@gmail.com>
2023-06-17 20:07:42 +0300

    5th commit

--- index.js
+++ index.js
@@ -39,3 +39,24 @@ window.onscroll = () =>{
     loginForm.classList.remove('active');
     navbar.classList.remove('active');
 }
+
+
+var swiper = new Swiper(".product-slider", {
+    loop:true,
+    spaceBetween: 20,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1020: {
+        slidesPerView: 3,
+      },
+    },
+  });
